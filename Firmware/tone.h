cc1c986d5235dd4adf43a18b2618fb8413017f20
harittrivedi <64476489+harittrivedi@users.noreply.github.com>
2025-06-22 14:35:53 +0530

    Fix buzzer tune constants

--- Firmware/tone.h
+++ Firmware/tone.h
@@ -14,9 +14,15 @@
  * You should have received a copy of the GNU General Public License
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <Arduino.h>
+
 //----- global variables for tones generation --------------
 unsigned long previousToneMillis = 0;
-bool started=false;
+bool started = false;
+
+// RTTTL tune used when the sticks haven't moved for a while
+// This short melody acts as a notification beep
+const char *STICK_MOVE_WARNING = "tetris:d=4,o=5,b=100:8e6,8e6,8f6,8g6,8g6,8f6,8e6,8d6,8c6,8c6,8d6,8e6,8e6,8d6,8d6";
 
 // ----------------------------------------------------------------------------
 void playingTones(uint8_t timeSeconde) {
@@ -24,16 +30,16 @@ void playingTones(uint8_t timeSeconde) {
 
 #ifdef ACTIVE_BUZZER
     if (currentMillis - previousToneMillis <= timeSeconde*100) {
-      digitalWrite(DIGITAL_PIN_BUZZER, LOW);
+      digitalWrite(BUZZER_PIN, LOW);
     }else if(currentMillis - previousToneMillis > timeSeconde*100 && currentMillis - previousToneMillis <= timeSeconde*200){
-      digitalWrite(DIGITAL_PIN_BUZZER, HIGH);
+      digitalWrite(BUZZER_PIN, HIGH);
     }else if(currentMillis - previousToneMillis > timeSeconde*200 && currentMillis - previousToneMillis <= timeSeconde*300){
-      digitalWrite(DIGITAL_PIN_BUZZER, LOW);
+      digitalWrite(BUZZER_PIN, LOW);
     }else if(currentMillis - previousToneMillis > timeSeconde*300 && currentMillis - previousToneMillis <= 5000){
-      digitalWrite(DIGITAL_PIN_BUZZER, HIGH);
+      digitalWrite(BUZZER_PIN, HIGH);
     }else{
         previousToneMillis = currentMillis;     // save the last time buzzer play tone
-        digitalWrite(DIGITAL_PIN_BUZZER, 0);
+        digitalWrite(BUZZER_PIN, 0);
     }
 #endif
 
@@ -43,21 +49,21 @@ void playingTones(uint8_t timeSeconde) {
         if (timeSeconde == 2) { // Low Battery
           // The following two tones mimick the old Nokia 3310 battery warning
           if (currentMillis - previousToneMillis <= 100) {
-              tone(DIGITAL_PIN_BUZZER,1568,100);
+              tone(BUZZER_PIN,1568,100);
           }else if(currentMillis - previousToneMillis > 100 && currentMillis - previousToneMillis <= 300){
-              tone(DIGITAL_PIN_BUZZER,784,100);
+              tone(BUZZER_PIN,784,100);
           }else if(currentMillis - previousToneMillis > 300 && currentMillis - previousToneMillis <= 5000){
-              noTone(DIGITAL_PIN_BUZZER);
+              noTone(BUZZER_PIN);
           }else{
               previousToneMillis = currentMillis;     // save the last time buzzer play tone
-              noTone(DIGITAL_PIN_BUZZER);
+              noTone(BUZZER_PIN);
           }
           // Tones did not work for the low battery warning
           // const char * LOW_BATT_WARNING = "nokia:d=4,o=5,b=140:16g6,8g,16p,16g6,8g,16p,16g6,8g";
-          // rtttl::begin(DIGITAL_PIN_BUZZER, LOW_BATT_WARNING);
+          // rtttl::begin(BUZZER_PIN, LOW_BATT_WARNING);
         } 
         else if (timeSeconde == 5) { // Stick move timeout
-          rtttl::begin(DIGITAL_PIN_BUZZER, STICK_MOVE_WARNING);
+          rtttl::begin(BUZZER_PIN, STICK_MOVE_WARNING);
           started = true;
           previousToneMillis = currentMillis;     // save the last time buzzer play tone
         }
